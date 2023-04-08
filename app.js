b909649352d0192232e7dd82c661d72069327f21
Cuongtran6969 <95119298+Cuongtran6969@users.noreply.github.com>
2023-04-08 22:43:21 +0700

    Update app.js

--- app.js
+++ app.js
@@ -21,4 +21,24 @@ let intervalId = setInterval(()=> {
       }
 
 }, 2000)
+//agter 2s increase progress and change bg color
+let isLoading = true;
+let intervalId = setInterval(()=> {
+    value+=10;
+    progressBar.style.width = value+"%";
+    progressBar.innerHTML =  value+"%";
+    if(value<30) {
+        progressBar.classList.add('bg-danger')
 
+    }else if(value >=30 && value <70) {
+        progressBar.classList.remove('bg-danger')
+        progressBar.classList.add('bg-warning')
+    }else{
+        progressBar.classList.remove('bg-warning')
+        progressBar.classList.add('bg-success')
+    }
+    if (value === 100) {
+        clearInterval(intervalId)
+      }
+
+}, 2000)
